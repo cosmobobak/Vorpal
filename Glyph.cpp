81af74baabbc5c654476216de75ebec0078f7d60
cosmobobak <56003038+cosmobobak@users.noreply.github.com>
2020-09-26 15:58:37 +0100

    Updated Glpyh to use bitboards.

--- Glyph.cpp
+++ Glyph.cpp
@@ -3,184 +3,292 @@
 
 class Glyph
 {
-    public:
-        char node[3][3] = {
-            {'.', '.', '.'}, 
-            {'.', '.', '.'}, 
-            {'.', '.', '.'}
-            };
-        int turn = 1;
-        int nodes = 0;
+public:
+    int node[2] = {0b000000000, 0b000000000};
+    int turn = 1;
+    int nodes = 0;
 
-        bool is_full()
+    void reset_nodes()
+    {
+        nodes = 0;
+    }
+
+    bool pos_filled(int i)
+    {
+        if ((node[0] & (1L << i)) != 0)
         {
-            for (int x = 0; x < 3; x++){
-                for (int y = 0; y < 3; y++){
-                    if (node[x][y] == '.'){
-                        return false;
-                    }
-                }
-            }
             return true;
         }
-
-        void show()
+        else if ((node[1] & (1L << i)) != 0)
         {
-            int x, y;
-            for (x = 0; x < 3; ++x){
-                for (y = 0; y < 3; ++y){
-                    std::cout << node[x][y] << ' ';
+            return true;
+        }
+        return false;
+    }
+
+    bool player_at(int i) //only valid to use if pos_filled() returns true
+    {
+        if ((node[0] & (1L << i)) != 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    bool is_full()
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            if (!pos_filled(i))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    void show()
+    {
+        for (int x = 0; x < 3; x++)
+        {
+            for (int y = 0; y < 3; y++)
+            {
+                if (pos_filled(x * 3 + y))
+                {
+                    if (player_at(x * 3 + y))
+                    {
+                        std::cout << "X ";
+                    }
+                    else
+                    {
+                        std::cout << "0 ";
+                    }
                 }
-                std::cout << '\n';
-            }
-            std::cout << '\n';
-        }
-
-        void play(int x, int y)
-        {
-            if (turn == 1){
-                node[x][y] = 'X';
-                turn = -1;
-            }else{
-                node[x][y] = '0';
-                turn = 1;
-            }
-        }
-
-        void unplay(int x, int y)
-        {
-            node[x][y] = '.';
-            if (turn == 1){turn = -1;} else {turn = 1;}
-        }
-
-        int evaluate()
-        {
-            for (int row = 0; row < 3; row++){
-                if (node[row][0] == node[row][1] && node[row][1] == node[row][2]){
-                    if (node[row][0] == 'X'){return 1;}
-                    else if (node[row][0] == '0'){return -1;}
+                else
+                {
+                    std::cout << ". ";
                 }
             }
-            for (int col = 0; col < 3; col++){
-                if (node[0][col] == node[1][col] && node[1][col] == node[2][col]){
-                    if (node[0][col] == 'X'){return 1;}
-                    else if (node[0][col] == '0'){return -1;}
+            std::cout << "\n";
+        }
+        std::cout << "\n";
+    }
+
+    void play(int i)
+    {
+        // n ^ (1 << k) is a binary XOR where you flip the kth bit of n
+        if (turn == 1)
+        {
+            node[0] = node[0] ^ (1 << i);
+            turn = -1;
+        }
+        else
+        {
+            node[1] = node[1] ^ (1 << i);
+            turn = 1;
+        }
+    }
+
+    void unplay(int i) //only valid directly after a move, do not unplay on root, or unplay twice in a row
+    {
+        if (turn == 1)
+        {
+            node[1] = node[1] ^ (1 << i);
+            turn = -1;
+        }
+        else
+        {
+            node[0] = node[0] ^ (1 << i);
+            turn = 1;
+        }
+    }
+
+    void pass()
+    {
+        if (turn == 1)
+        {
+            turn = -1;
+        }
+        else
+        {
+            turn = 1;
+        }
+    }
+
+    int evaluate()
+    {
+        nodes++; // increment nodes
+        // check first diagonal
+        if (pos_filled(0) && pos_filled(4) && pos_filled(8))
+        {
+            if (player_at(0) == player_at(4) && player_at(4) == player_at(8))
+            {
+                if (player_at(0))
+                {
+                    return 1;
+                }
+                else
+                {
+                    return -1;
                 }
             }
-            if (node[0][0] == node[1][1] && node[1][1] == node[2][2]){
-                if (node[0][0] == 'X'){return 1;}
-                else if (node[0][0] == '0'){return -1;}
-            }
-            if (node[0][2] == node[1][1] && node[1][1] == node[2][0]){
-                if (node[0][2] == 'X'){return 1;}
-                else if (node[0][2] == '0'){return -1;}
-            }
-            return 0;
         }
-
-        int negamax(int colour, int a = -20, int b = 20)
+        // check second diagonal
+        if (pos_filled(2) && pos_filled(4) && pos_filled(6))
         {
-            if(evaluate() != 0 || is_full() == true){return turn * evaluate();}
-            int score;
-            int x, y;
-
-            for (int x = 0; x < 3; x++){
-                for (int y = 0; y < 3; y++){
-                    if (node[x][y] == '.'){
-                        play(x, y);
-                        nodes++;
-                        score = -negamax(-colour, -b, -a);
-                        unplay(x, y);
-
-                        if (score > b){return b;}
-                        if (score > a){a = score;}
+            if (player_at(2) == player_at(4) && player_at(4) == player_at(6))
+            {
+                if (player_at(2))
+                {
+                    return 1;
+                }
+                else
+                {
+                    return -1;
+                }
+            }
+        }
+        // check rows
+        for (int i = 0; i < 3; i++)
+        {
+            if (pos_filled(i * 3) && pos_filled(i * 3 + 1) && pos_filled(i * 3 + 2))
+            {
+                if (player_at(i * 3) == player_at(i * 3 + 1) && player_at(i * 3 + 1) == player_at(i * 3 + 2))
+                {
+                    if (player_at(i * 3))
+                    {
+                        return 1;
+                    }
+                    else
+                    {
+                        return -1;
                     }
                 }
             }
-            return a;
         }
-
-        int max_pos(int arr[])
+        // check columns
+        for (int i = 0; i < 3; i++)
         {
-            int max, index;
-            max = arr[0];
-            index = 0;
-            for (int i = 0; i < 9; i++){
-                if (arr[i] > max){
-                    max = arr[i];
-                    index = i;
-                }
-            }
-            return index;
-        }
-
-        int min_pos(int arr[])
-        {
-            int min, index;
-            min = arr[0];
-            index = 0;
-            for (int i = 0; i < 9; i++){
-                if (arr[i] < min){
-                    min = arr[i];
-                    index = i;
-                }
-            }
-            return index;
-        }
-
-        void engine_move()
-        {
-            int x, y, index;
-            int scores [9] = {-9,-9,-9,-9,-9,-9,-9,-9,-9};
-            
-            for (int row = 0; row < 3; row++){
-                for (int col = 0; col < 3; col++){
-                    if (node[row][col] == '.'){
-                        play(row, col);
-                        scores[row*3+col] = -negamax(turn);
-                        unplay(row, col);
+            if (pos_filled(i) && pos_filled(i + 3) && pos_filled(i + 6))
+            {
+                if (player_at(i) == player_at(i + 3) && player_at(i + 3) == player_at(i + 6))
+                {
+                    if (player_at(i))
+                    {
+                        return 1;
+                    }
+                    else
+                    {
+                        return -1;
                     }
                 }
             }
-            //cout << '{'; for (int i = 0; i < 9; i++){cout << scores[i] << ", ";}; cout << '}';
-            //cout << '\n';
-            index = max_pos(scores);
-            x = index/3;
-            y = index%3;
-            play(x, y);
+        }
+        return 0;
+    }
+
+    int negamax(int turn, int a = -2, int b = 2)
+    {
+        if (evaluate() != 0 || is_full() == true)
+        {
+            return turn * evaluate();
         }
 
-        void show_result()
+        int score;
+        for (int i = 0; i < 9; i++)
         {
-            int r;
-            r = evaluate();
-            if (r == 0)
+            if (!pos_filled(i))
             {
-                std::cout << "1/2-1/2" << '\n';
-            }
-            else if (r == 1)
-            {
-                std::cout << "1-0" << '\n';
-            }
-            else
-            {
-                std::cout << "0-1" << '\n';
+                play(i);
+                score = -negamax(-turn, -b, -a);
+                unplay(i);
+
+                if (score > b)
+                {
+                    return b;
+                }
+                if (score > a)
+                {
+                    a = score;
+                }
             }
         }
+        return a;
+    }
+
+    int max_pos(int arr[])
+    {
+        int max, index;
+        max = arr[0];
+        index = 0;
+        for (int i = 0; i < 9; i++)
+        {
+            if (arr[i] > max)
+            {
+                max = arr[i];
+                index = i;
+            }
+        }
+        return index;
+    }
+
+    void engine_move()
+    {
+        int index;
+        int scores[9] = {-2, -2, -2, -2, -2, -2, -2, -2, -2};
+
+        for (int i = 0; i < 9; i++)
+        {
+            if (!pos_filled(i))
+            {
+                play(i);
+                scores[i] = -negamax(turn, -2, 2);
+                unplay(i);
+            }
+        }
+        index = max_pos(scores);
+        play(index);
+    }
+
+    void show_result()
+    {
+        int r;
+        r = evaluate();
+        if (r == 0)
+        {
+            std::cout << "1/2-1/2" << '\n';
+        }
+        else if (r == 1)
+        {
+            std::cout << "1-0" << '\n';
+        }
+        else
+        {
+            std::cout << "0-1" << '\n';
+        }
+    }
 };
 
 int main()
 {
     Glyph glyph;
-    int x, y;
+    int i;
+    glyph.show();
     while (glyph.evaluate() == 0 && glyph.is_full() == false)
     {
         glyph.engine_move();
+        std::cout << "Nodes processed for move: " << glyph.nodes << "\n";
+        glyph.reset_nodes();
         glyph.show();
-        if (glyph.evaluate() != 0 || glyph.is_full() == true){break;}
-        std::cin >> x;
-        std::cin >> y;
-        glyph.play(x, y);
+        if (glyph.evaluate() != 0 || glyph.is_full() == true)
+        {
+            break;
+        }
+        std::cin >> i;
+        glyph.play(i);
         glyph.show();
     }
     glyph.show_result();
